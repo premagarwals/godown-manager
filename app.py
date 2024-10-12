8bac4c46caacda1b746eb9178498de40b75f3735
premagarwals <prem12937@gmail.com>
2024-10-13 00:39:40 +0530

    feat: add jwt authentication

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import json
+import os
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app)
+SECRET_KEY = os.getenv('JWT_KEY')
+ADMIN_ALIAS = os.getenv('ADMIN_ALIAS')
+ADMIN_PASSKEY = os.getenv('ADMIN_KEY')
+if not all([SECRET_KEY,ADMIN_ALIAS,ADMIN_PASSKEY]):
+    raise EnvironmentError("\n\nEnvironment variables not found!\nrequired variables: ADMIN_ALIAS, ADMIN_KEY, JWT_KEY\n\nPlease run: export <variable>=<value>\n\nDon't get afraid, you can set any value you want :)\n")
 
 def load_godowns():
     with open('godown.json', 'r') as file:
@@ -65,6 +73,49 @@ def get_item_data(id):
     else:
         abort(404)
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    
+    if 'passKey' not in data:
+        return jsonify({'auth': False, 'message': 'passKey not provided'})#, 400
+
+    alias = data['alias']
+    passKey = data['passKey']
+
+    if passKey == ADMIN_PASSKEY and alias == ADMIN_ALIAS:
+        token = jwt.encode({
+            'user': ADMIN_ALIAS, 
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'auth': True, 'token': token, 'message': 'Login successful'})
+
+    return jsonify({'auth': False, 'message': 'Invalid Alias or Passkey'})#, 403
+
+@app.route('/verify', methods=['POST'])
+def verify():
+    data = request.get_json()
+    token = data.get('token')
+
+    if not token:
+        return jsonify({'auth': False, 'message': 'Token not provided'})#, 401
+
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+
+        if decoded.get("user") == ADMIN_ALIAS:
+            return jsonify({'auth': True})
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'auth': False, 'message': 'Token has expired'})#, 401
+    except jwt.InvalidTokenError:
+        return jsonify({'auth': False, 'message': 'Invalid token'})#, 401
+
+    return jsonify({'auth': False, 'message': 'User is not admin'})#, 403
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
